b9aba114cc6dcf3aa52ced49f545a01ff4781f40
张俊杰 <zhangjunjie@zhangjunjies-MacBook-Pro.local>
2022-04-05 09:53:11 -0400

    update

--- js/video.js
+++ js/video.js
@@ -1,11 +1,46 @@
-var video;
+const playButton = document.querySelector("#play");
+const pauseButton = document.querySelector("#pause");
+const slowerButton = document.querySelector("#slower");
+const fasterButton = document.querySelector("#faster");
+const skipButton = document.querySelector("#skip");
+const player = document.querySelector("#player1");
+const muteButton = document.querySelector("#mute");
+const slider = document.querySelector("#slider");
+const origStyleButton = document.querySelector("#orig");
+const vintageStyleButton = document.querySelector("#vintage");
+const volume = document.querySelector("#volume");
 
-window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
+window.addEventListener("load", function () {
+  console.log("Good job opening the window");
+  player.loop = false;
+  player.preload = true;
+  player.autoplay = false;
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+playButton.addEventListener("click", function () {
+  player.play();
+  volume.innerHTML = player.volume * 100 + "%";
+});
 
+pauseButton.addEventListener("click", function () {
+  player.pause();
+});
+
+slowerButton.addEventListener("click", function () {
+  player.playbackRate *= 0.95;
+  console.log(play.playbackRate);
+});
+
+fasterButton.addEventListener("click", function () {
+  player.playbackRate /= 0.95;
+  console.log(play.playbackRate);
+});
+
+slider.addEventListener("change", function (e) {
+  if (mute.innerHTML == "Unmute") {
+    media.muted = false;
+    mute.innerHTML = "Mute";
+  }
+  volume.innerHTML = e.target.value + "%";
+  player.volume = e.target.value / 100;
+});
